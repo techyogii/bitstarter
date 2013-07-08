8eb152501efb06793bb1ecb2f6ef8729c4207ef4
Ubuntu <ubuntu@ip-10-164-67-164.ec2.internal>
2013-07-08 18:54:38 +0000

    king

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Universe!');
+  response.send('Hello Universe! I am King !');
 });
 
 var port = process.env.PORT || 5000;
