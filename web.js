fd86d681351953463865668c532b1e6d50a13cc4
techyogii <yogi.hammerhead@gmail.com>
2013-07-09 17:48:10 +0000

    fdsf

--- web.js
+++ web.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+var app = express.createServer(express.logger());
+
+fs=require('fs');
+
+app.get('/', function(request, response) {
+  response.send('Hello World22push!');
+
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
