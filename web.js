7bb1432933a154c3b8ae88f92a00d50ae8e9bbd8
techyogii <yogi.hammerhead@gmail.com>
2013-07-09 17:31:25 +0000

    edit

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs=require('fs');
-
+var fs=require('fs');
+var http=require('http');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
 app.get('/', function(request, response) {
   response.send('Hello World22push!');
 
