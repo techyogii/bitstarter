25dcdf714f1126bbad829006b5ae8f55ba25063c
techyogii <yogi.hammerhead@gmail.com>
2013-07-09 18:20:59 +0000

    fdsf

--- web.js
+++ web.js
@@ -2,10 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs=require('fs');
+var fs=require('fs');
 
 app.get('/', function(request, response) {
   response.send('Hello World22push!');
+fs.readFileSync('./index.html',function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 });
 
