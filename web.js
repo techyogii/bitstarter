439c044448325a0cfc26ee0f0ac4728cbcb03b2e
techyogii <yogi.hammerhead@gmail.com>
2013-07-09 17:45:39 +0000

    afdas

--- web.js
+++ web.js
@@ -1,18 +1,8 @@
 var express = require('express');
+var app = express();
 
-var app = express.createServer(express.logger());
-
-var fs=require('fs');
-var http=require('http');
-
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
+app.listen(3000);
